b7bf4b728f29a4d9a47e5c689bbb4926f4d19a52
RCLQD <reysilclarion17@gmail.com>
2024-09-23 20:55:12 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Home from './pages/home'
+import Navbar from './pages/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  )
+}
+
+export default App
